6f54416c7fd6281cdc88e84c3ddc1b050f09edbf
superminsu <alstn6870@gmail.com>
2022-11-10 15:05:00 +0900

    초기 설정 구축

--- src/main/java/kr/inhatc/spring/CapstoneManagementApplication.java
+++ src/main/java/kr/inhatc/spring/CapstoneManagementApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MinsuShopApplication {
+public class CapstoneManagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MinsuShopApplication.class, args);
+		SpringApplication.run(CapstoneManagementApplication.class, args);
 	}
 
 }
